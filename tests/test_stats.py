f8fd24bbea846dececb2cb6c914c69266a34e235
Camille Scott <camille.scott.w@gmail.com>
2017-02-16 11:54:35 -0800

    Test dummy PSequence

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -3,6 +3,7 @@ import pytest
 #from .utils import *
 import khmer
 from khmer._oxli.hashing import Kmer
+from khmer._oxli.parsing import Sequence
 
 from boink.graph import ProbabilisticGraph
 from boink.stats import PSequenceFunction, PKmerFunction, PFunction_shim
@@ -13,3 +14,8 @@ def test_PKmerFunc():
     f = PKmerFunction(g)
     assert f.evaluate(kmer) == 1.0
 
+def test_PSequenceFunc():
+    sequence = Sequence(name='test', sequence='AAATAGCTAGCTCGATCGATCGGG')
+    g = khmer.Nodegraph(5, 1e4, 4)
+    f = PSequenceFunction(g)
+    assert f.evaluate(sequence) == 1.0
