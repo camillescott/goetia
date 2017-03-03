f1549f174bbad1643f1c0e92b29cd77fd5ce29c7
Camille Scott <camille.scott.w@gmail.com>
2017-03-02 17:28:17 -0800

    Test counting methods and loop

--- boink/dbg_tests.pyx
+++ boink/dbg_tests.pyx
@@ -9,6 +9,7 @@ from libcpp.string cimport string
 from lemon cimport SmartDigraph, Node, NodeIt, Arc, INVALID
 from dbg import ExactDBG
 from dbg cimport ExactDBG
+from utils cimport _bstring
 
 
 cdef ExactDBG get_test_graph(string s, int K):
@@ -50,3 +51,21 @@ cpdef _test_kmer_degree():
     cdef ExactDBG g = get_test_graph(s, 4)
     assert g._kmer_degree(s.substr(0,4)) == 1
     assert g._kmer_degree(s.substr(1,4)) == 1
+
+cpdef _test_kmer_count():
+    cdef string s = 'AAAA'
+    cdef ExactDBG g = get_test_graph(s, 4)
+
+    for i in range(1, 10):
+        assert g._kmer_count(s) == i
+        g._add_kmer(s)
+
+cpdef _test_add_loop(seq, K):
+    cdef string s = _bstring(seq)
+    cdef ExactDBG g = get_test_graph(s, K)
+    #print([n.kmer for n in g.nodes()])
+
+    for i in range(0,s.length()-K+1):
+        d = g._kmer_degree(s.substr(i,K))
+        #print(s.substr(i,K))
+        assert d == 2, (d, g._n_nodes())
