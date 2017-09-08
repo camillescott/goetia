e8293a87bb634a70082389d8aaab61727bfbe096
Camille Scott <camille.scott.w@gmail.com>
2017-09-08 00:40:01 -0700

    Remove some unused code

--- boink/lemon.pyx
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-# cython: c_string_type=unicode, c_string_encoding=utf8
-
-from cython.operator cimport dereference as deref
-from cython.operator cimport preincrement as preinc
-from libcpp.memory cimport unique_ptr, weak_ptr, shared_ptr
-
-
-
