1549235a330ecdb8ba1e7a13d74985425be94920
Camille Scott <camille.scott.w@gmail.com>
2018-04-18 18:35:22 -0700

    First pass at SplitPairedReader cython wrapper

---
+++ boink/parsing.pxd
@@ -0,0 +1,41 @@
+# boink/parsing.pxd
+# Copyright (C) 2018 Camille Scott
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
+from libc.stdint cimport uint16_t, uint32_t, uint64_t, int64_t
+from libcpp cimport bool
+from libcpp.memory cimport unique_ptr
+from libcpp.string cimport string
+
+from khmer._oxli.parsing cimport CpSequence as _Sequence
+
+
+cdef extern from "boink/parsing.hh" namespace "boink" nogil:
+
+    cdef struct _SequenceBundle "boink::ReadBundle":
+        bool has_left
+        bool has_right
+        _Sequence left
+        _Sequence right
+
+    cdef cppclass _SplitPairedReader "boink::SplitPairedReader" [ParserType=*]:
+        _SplitPairedReader(const string&,
+                           const string&,
+                           uint32_t,
+                           bool)
+
+        _SplitPairedReader(const string&,
+                           const string&)
+
+        bool is_complete()
+        _SequenceBundle next()
+
+cdef class SplitPairedReader:
+
+    cdef unique_ptr[_SplitPairedReader] _this
+    cdef readonly int min_length
+    cdef readonly bool force_name_match
+
---
+++ boink/parsing.pyx
@@ -0,0 +1,34 @@
+# boink/parsing.pyx
+# Copyright (C) 2018 Camille Scott
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
+from cython.operator cimport dereference as deref
+from libcpp.memory cimport make_unique
+
+from boink.utils cimport _bstring
+
+from khmer._oxli.parsing cimport Sequence
+
+cdef class SplitPairedReader:
+
+    def __init__(self, str left_filename, str right_filename,
+                       int min_length=0, bool force_name_match=False):
+        self._this = make_unique[_SplitPairedReader](_bstring(left_filename),
+                                                     _bstring(right_filename),
+                                                     min_length,
+                                                     force_name_match)
+
+    def __iter__(self):
+        cdef _SequenceBundle bundle
+        cdef read_num = 0
+
+        while not deref(self._this).is_complete():
+            bundle = deref(self._this).next()
+            yield (read_num, True,
+                   Sequence._wrap(bundle.left),
+                   Sequence._wrap(bundle.right))
+
+            read_num += (<int>bundle.has_left + <int>bundle.has_right)
