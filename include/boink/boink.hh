3f4868e071067b0a8fc9507c81a943bfcd7dff2b
Camille Scott <camille.scott.w@gmail.com>
2018-08-28 16:31:26 -0700

    Fix uint comparison warning

--- include/boink/boink.hh
+++ include/boink/boink.hh
@@ -91,7 +91,7 @@ template <typename T>
 std::ostream& operator<<(std::ostream& os, const vector<T>& v)
 {
     os << "[";
-    for (int i = 0; i < v.size(); ++i) {
+    for (size_t i = 0; i < v.size(); ++i) {
         os << v[i];
         if (i != v.size() - 1)
             os << ", ";
