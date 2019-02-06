898af01f5c3814ff4eac8b0a88d5c14f31f87974
Camille Scott <camille.scott.w@gmail.com>
2019-02-05 18:54:13 -0800

    update scripts

--- scripts/track-decision-nodes.py
+++ scripts/track-decision-nodes.py
@@ -3,13 +3,14 @@
 import argparse
 import sys
 
-from boink.args import build_dBG_args
+from boink.args import build_dBG_args, add_output_interval_args
 from boink.dbg import make_dBG
-from boink.cdbg import StreamingCompactor
-from boink.processors import DecisionNodeProcessor
+from boink.compactor import make_streaming_compactor
+from boink.processors import make_decision_node_processor
 
 def parse_args():
     parser = build_dBG_args()
+    add_output_interval_args(parser)
     parser.add_argument('-o', dest='output_filename', default='/dev/stdout')
     parser.add_argument('-i', dest='inputs', nargs='+', default=['/dev/stdin'])
     parser.add_argument('--output-interval', type=int, default=10000)
@@ -23,12 +24,14 @@ def main():
 
     graph = make_dBG(args.ksize,
                      args.max_tablesize,
-                     args.n_tables)
-    compactor = StreamingCompactor(graph)
-    processor = DecisionNodeProcessor(compactor,
-                                      args.output_filename,
-                                      args.output_interval)
-
+                     args.n_tables,
+                     storage='_' + args.storage_type)
+    compactor = make_streaming_compactor(graph)
+    processor = make_decision_node_processor(compactor,
+                                             args.output_filename,
+                                             args.fine_interval,
+                                             args.medium_interval,
+                                             args.coarse_interval)
     for filename in args.inputs:
         processor.process(filename)
 
