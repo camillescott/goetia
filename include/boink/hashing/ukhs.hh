b44b19cb56ce50e3c973dfc16b7a03539caf1206
Camille Scott <camille.scott.w@gmail.com>
2019-10-10 00:13:16 -0700

    missed one stdout call

--- include/boink/hashing/ukhs.hh
+++ include/boink/hashing/ukhs.hh
@@ -357,7 +357,7 @@ struct UKHS {
 
         Unikmer get_min_unikmer() {
             //std::cout << "LazyShifter: " << repr(window_unikmers) << std::endl;
-            std::cout << "LazyShifter: " << repr(unikmer_indices) << std::endl;
+            //std::cout << "LazyShifter: " << repr(unikmer_indices) << std::endl;
 
             if (window_unikmers.size() == 0) {
                 throw BoinkException("Window should contain unikmer.");
