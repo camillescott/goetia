4be38aea9b191d6b3d091ea822eefba837fe79b7
Camille Scott <camille.scott.w@gmail.com>
2020-02-26 13:05:57 -0800

    ADd test for scaled

--- tests/test_signatures.py
+++ tests/test_signatures.py
@@ -30,3 +30,23 @@ def test_sourmash_signature(datadir, ksize):
     boink_mh = boink_sig.to_sourmash()
 
     assert boink_mh.similarity(sourmash_sig) == 1.0
+
+
+def test_sourmash_scaled(datadir, ksize):
+    import sourmash
+
+    rfile = datadir('random-20-a.fa')
+    boink_sig = SourmashSignature.Signature.build(0, 31, False, False, False, 42, 1000)
+    sourmash_sig = sourmash.MinHash(0, 31, scaled=1000)
+
+    processor = SourmashSignature.Processor.build(boink_sig)
+    processor.process(rfile)
+
+    for record in screed.open(rfile):
+        sourmash_sig.add_sequence(record.sequence)
+
+    boink_mh = boink_sig.to_sourmash()
+
+    assert boink_mh.similarity(sourmash_sig) == 1.0
+
+
