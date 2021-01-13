41e9ddbfd4db2316d2607cb2e7e792ce9785dd36
Camille Scott <camille.scott.w@gmail.com>
2021-01-13 04:31:28 -0800

    cDBG callbacks do not use outdated interval tracking method

--- goetia/cdbg.py
+++ goetia/cdbg.py
@@ -19,93 +19,88 @@ async def compute_connected_component_callback(msg,
                                                cdbg_type,
                                                cdbg,
                                                out_file,
-                                               sample_size,
-                                               interval='coarse'):
-    if interval in msg.state:
-        result = cdbg_type.compute_connected_component_metrics(cdbg,
-                                                               sample_size)
-        n_comps, min_comp, max_comp, size_dist = result
+                                               sample_size):
+        
+    result = cdbg_type.compute_connected_component_metrics(cdbg,
+                                                            sample_size)
+    n_comps, min_comp, max_comp, size_dist = result
 
-        data = {'t': msg.t,
-                'sample_name': msg.sample_name,
-                'n_components': n_comps,
-                'max': max_comp,
-                'min': min_comp,
-                'size_dist': size_dist}
+    data = {'t': msg.t,
+            'sample_name': msg.sample_name,
+            'n_components': n_comps,
+            'max': max_comp,
+            'min': min_comp,
+            'size_dist': size_dist}
 
-        async with curio.aopen(out_file, 'a') as fp:
-            if await fp.tell() != 0:
-                await fp.write(',\n')
-            else:
-                await fp.write('[\n')
-            await fp.write(json.dumps(data))
+    async with curio.aopen(out_file, 'a') as fp:
+        if await fp.tell() != 0:
+            await fp.write(',\n')
+        else:
+            await fp.write('[\n')
+        await fp.write(json.dumps(data))
 
 
 async def compute_unitig_fragmentation_callback(msg,
                                                 cdbg_type,
                                                 cdbg,
                                                 out_file,
-                                                bins,
-                                                interval='medium'):
+                                                bins):
     
-    if interval in msg.state:
-        counts = cdbg_type.compute_unitig_fragmentation(cdbg,
-                                                        bins)
-        data = {'t': msg.t,
-                'sample_name': msg.sample_name}
-        for i in range(len(bins) - 1):
-            bin_start, bin_end = bins[i], bins[i+1]
-            data[f'[{bin_start},{bin_end})'] = counts[i]
-        data[f'[{bin_end},Inf)'] = counts[-1]
+    counts = cdbg_type.compute_unitig_fragmentation(cdbg,
+                                                    bins)
+    data = {'t': msg.t,
+            'sample_name': msg.sample_name}
+    for i in range(len(bins) - 1):
+        bin_start, bin_end = bins[i], bins[i+1]
+        data[f'[{bin_start},{bin_end})'] = counts[i]
+    data[f'[{bin_end},Inf)'] = counts[-1]
 
-        async with curio.aopen(out_file, 'a') as fp:
-            if await fp.tell() != 0:
-                await fp.write(',\n')
-            else:
-                await fp.write('[\n')
-            await fp.write(json.dumps(data))
+    async with curio.aopen(out_file, 'a') as fp:
+        if await fp.tell() != 0:
+            await fp.write(',\n')
+        else:
+            await fp.write('[\n')
+        await fp.write(json.dumps(data))
 
 
 async def write_cdbg_metrics_callback(msg,
                                       compactor,
-                                      out_file,
-                                      interval='fine'):
-    if interval in msg.state:
-        report = compactor.get_report()
-        data = {'t': msg.t,
-                'sample_name': msg.sample_name,
-                'n_full': report.n_full,
-                'n_tips': report.n_tips,
-                'n_islands': report.n_islands,
-                'n_trivial': report.n_trivial,
-                'n_circular': report.n_circular,
-                'n_loops': report.n_loops,
-                'n_dnodes': report.n_dnodes,
-                'n_tags': report.n_tags,
-                'n_updates': report.n_updates,
-                'n_splits': report.n_splits,
-                'n_merges': report.n_merges,
-                'n_extends': report.n_extends,
-                'n_clips': report.n_clips,
-                'n_deletes': report.n_deletes,
-                'n_circular_merges': report.n_circular_merges,
-                'n_unique_kmers': report.n_unique,
-                'estimated_fp': report.estimated_fp}
+                                      out_file):
 
-        async with curio.aopen(out_file, 'a') as fp:
-            if await fp.tell() != 0:
-                await fp.write(',\n')
-            else:
-                await fp.write('[\n')
-            await fp.write(json.dumps(data))
+    report = compactor.get_report()
+    data = {'t': msg.t,
+            'sample_name': msg.sample_name,
+            'n_full': report.n_full,
+            'n_tips': report.n_tips,
+            'n_islands': report.n_islands,
+            'n_trivial': report.n_trivial,
+            'n_circular': report.n_circular,
+            'n_loops': report.n_loops,
+            'n_dnodes': report.n_dnodes,
+            'n_tags': report.n_tags,
+            'n_updates': report.n_updates,
+            'n_splits': report.n_splits,
+            'n_merges': report.n_merges,
+            'n_extends': report.n_extends,
+            'n_clips': report.n_clips,
+            'n_deletes': report.n_deletes,
+            'n_circular_merges': report.n_circular_merges,
+            'n_unique_kmers': report.n_unique,
+            'estimated_fp': report.estimated_fp}
+
+    async with curio.aopen(out_file, 'a') as fp:
+        if await fp.tell() != 0:
+            await fp.write(',\n')
+        else:
+            await fp.write('[\n')
+        await fp.write(json.dumps(data))
 
 
 async def write_cdbg_callback(msg,
                               cdbg,
                               out_file_prefix,
-                              out_format,
-                              interval='coarse'):
-    if interval in msg.state:
-        out_file_name = f'{out_file_prefix}.{msg.t}.{out_format}'
-        cdbg.write(out_file_name,
-                   cDBGSerialization.enum_from_str(out_format))
+                              out_format):
+
+    out_file_name = f'{out_file_prefix}.{msg.t}.{out_format}'
+    cdbg.write(out_file_name,
+                cDBGSerialization.enum_from_str(out_format))
