6ec72e35a369823cef98eb588aa1e5505bd2f76d
Camille Scott <camille.scott.w@gmail.com>
2021-10-08 17:15:49 -0700

    Conform filter subcommands to StatusOutput

--- goetia/cli/diginorm.py
+++ goetia/cli/diginorm.py
@@ -7,6 +7,7 @@
 # Date   : 08.10.2021
 
 import sys
+import time
 
 from goetia.filters import DiginormFilter
 from goetia.dbg import get_graph_args, process_graph_args
@@ -14,6 +15,9 @@ from goetia.cli.args import get_output_interval_args
 from goetia.cli.runner import CommandRunner
 from goetia.parsing import get_fastx_args, iter_fastx_inputs
 from goetia.storage import get_storage_args, process_storage_args
+from goetia.utils import Counter
+
+import blessings
 
 
 desc = '''
@@ -53,12 +57,46 @@ class DiginormFilterRunner(CommandRunner):
         self.processor = self.filter_t.Processor.build(self.filter.__smartptr__(),
                                                        args.output_filename,
                                                        args.interval)
+        self.status = DiginormFilterRunner.StatusOutput(term=self.term)
+
+    class StatusOutput:
+
+        def __init__(self, term=None, file=sys.stderr):
+            self.file = file
+            self.term = term or blessings.Terminal()
+
+        def print(self, *args, **kwargs):
+            print(*args, **kwargs, file=self.file)
+
+        def start_sample(self, sample_name, file_names):
+            self.print(f'{self.term.italic}Begin sample: {self.term.normal}{sample_name}')
+            files = '\n'.join(['    ' + f for f in file_names])
+            self.print(f'{files}')
+
+            self.counter = Counter(0)
+            self.start_s = time.perf_counter()
+
+        def finish_sample(self):
+            elapsed_s = time.perf_counter() - self.start_s
+            self.print(f'    {self.term.italic}Finshed: {self.term.normal}{elapsed_s:0.4f}s')
+            del self.counter
+
+        def update(self, t, sequence_t, n_passed):
+            term = self.term
+            if self.counter != 0:
+                 self.print(term.move_up * 3, term.clear_eos, term.move_x(0), end='')
+            self.print(f'       {term.bold}sequence:          {term.normal}{sequence_t:,}')
+            self.print(f'       {term.bold}k-mers:            {term.normal}{t:,}')
+            self.print(f'       {term.bold}sequences passed:  {term.normal}{n_passed:,}', end='')
+            self.print(f' ({n_passed / sequence_t * 100.0:2f}%)')
+            self.counter += 1
 
     def execute(self, args):
         for sample, name in iter_fastx_inputs(args.inputs, args.pairing_mode, names=args.names):
+            self.status.start_sample(name, sample)
             for n_seqs, time, n_skipped in self.processor.chunked_process(*sample):
-                print(f'{sample}, {name}: {n_seqs} reads, {self.processor.n_passed()} passed filter.',
-                      file=sys.stderr)
+                self.status.update(time, n_seqs, self.processor.n_passed())
+            self.status.finish_sample()
 
     def teardown(self):
         pass
