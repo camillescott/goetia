1220384ff2fe189814766386bbffecccb4a31b37
Camille Scott <camille.scott.w@gmail.com>
2022-03-03 08:45:19 -0800

    improve diginorm statusoutput add add metrics saving option

--- goetia/cli/diginorm.py
+++ goetia/cli/diginorm.py
@@ -13,9 +13,11 @@ from goetia.filters import DiginormFilter
 from goetia.dbg import get_graph_args, process_graph_args
 from goetia.cli.args import get_output_interval_args
 from goetia.cli.runner import CommandRunner
+from goetia.cli.status import StatusOutput
 from goetia.parsing import get_fastx_args, iter_fastx_inputs
+from goetia.processors import JSONStreamWriter
 from goetia.storage import get_storage_args, process_storage_args
-from goetia.utils import Counter
+from goetia.utils import Counter, time_iterable
 
 import blessings
 
@@ -40,6 +42,7 @@ class DiginormFilterRunner(CommandRunner):
         group.add_argument('--quiet', action='store_true', default=False)
 
         parser.add_argument('-C', '--cutoff', type=int, default=10)
+        parser.add_argument('--metrics', nargs='?', const='goetia.diginorm.metrics.json')
         
         super().__init__(parser, description=desc)
 
@@ -47,6 +50,8 @@ class DiginormFilterRunner(CommandRunner):
         process_graph_args(args)
 
     def setup(self, args):
+        if args.metrics:
+            self.metrics_stream = JSONStreamWriter(args.metrics)
         self.dbg_t       = args.graph_t
         self.hasher      = args.hasher_t(args.ksize)
         self.storage     = args.storage.build(*args.storage_args)
@@ -58,48 +63,40 @@ class DiginormFilterRunner(CommandRunner):
         self.processor = self.filter_t.Processor.build(self.filter.__smartptr__(),
                                                        args.output_filename,
                                                        args.interval)
-        self.status = DiginormFilterRunner.StatusOutput(term=self.term, quiet=args.quiet)
-
-    class StatusOutput:
-
-        def __init__(self, term=None, file=sys.stderr, quiet=False):
-            self.file = file
-            self.term = term or blessings.Terminal()
-            self.quiet = quiet
-
-        def print(self, *args, **kwargs):
-            if not self.quiet:
-                print(*args, **kwargs, file=self.file)
-
-        def start_sample(self, sample_name, file_names):
-            self.print(f'{self.term.italic}Begin sample: {self.term.normal}{sample_name}')
-            files = '\n'.join(['    ' + f for f in file_names])
-            self.print(f'{files}')
-
-            self.counter = Counter(0)
-            self.start_s = time.perf_counter()
-
-        def finish_sample(self):
-            elapsed_s = time.perf_counter() - self.start_s
-            self.print(f'    {self.term.italic}Finshed: {self.term.normal}{elapsed_s:0.4f}s')
-            del self.counter
-
-        def update(self, t, sequence_t, n_passed):
-            term = self.term
-            if self.counter != 0:
-                 self.print(term.move_up * 3, term.clear_eos, term.move_x(0), end='')
-            self.print(f'       {term.bold}sequence:          {term.normal}{sequence_t:,}')
-            self.print(f'       {term.bold}k-mers:            {term.normal}{t:,}')
-            self.print(f'       {term.bold}sequences passed:  {term.normal}{n_passed:,}', end='')
-            self.print(f' ({n_passed / sequence_t * 100.0:2f}%)')
-            self.counter += 1
+        self.status = StatusOutput(['unique k-mers', 'sequences passed', 'estimated fp-rate'], term=self.term)
 
     def execute(self, args):
-        for sample, name in iter_fastx_inputs(args.inputs, args.pairing_mode, names=args.names):
+        warned_fp = False
+        for (sample, name), sample_start_time, _ in \
+            time_iterable(iter_fastx_inputs(args.inputs, args.pairing_mode, names=args.names)):
+
             self.status.start_sample(name, sample)
-            for n_seqs, time, n_skipped in self.processor.chunked_process(*sample):
-                self.status.update(time, n_seqs, self.processor.n_passed())
-            self.status.finish_sample()
+            for (n_seqs, stream_time, n_skipped), \
+                 interval_start_time, interval_elapsed_time in \
+                 time_iterable(self.processor.chunked_process(*sample)):
+                
+                n_passed = self.processor.n_passed()
+                p_passed = n_passed / n_seqs
+                passed = f'{n_passed:,} ({p_passed * 100.0:.1f}%)'
+                fp_rate = self.dbg.estimated_fp()
+                self.status.update(stream_time, n_seqs, interval_elapsed_time, self.dbg.n_unique(), passed, fp_rate)
+
+                if args.metrics:
+                    self.metrics_stream.write({'t': stream_time,
+                                               'seq_t': n_seqs,
+                                               'rt_elapsed_interval': interval_elapsed_time,
+                                               'n_seqs_passed': n_passed,
+                                               'p_seqs_passed': p_passed,
+                                               'n_unique_kmers': self.dbg.n_unique(),
+                                               'estimated_fp': fp_rate})
+                
+                if fp_rate >= 0.8 and warned_fp is False:
+                    self.status.message(f'WARNING: false positive rate has surpassed 0.8 ({fp_rate:.7f})')
+                    warned_fp = True
+
+
+            sample_elapsed_time = time.perf_counter() - sample_start_time
+            self.status.finish_sample(sample_elapsed_time)
 
     def teardown(self):
         pass
